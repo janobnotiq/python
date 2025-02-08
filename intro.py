9a41a5c28d5442038f0586baeb5582c510e47e9a
Ulugbek <janobnotiq@gmail.com>
2025-02-08 23:29:47 +0500

    intro

---
+++ intro.py
@@ -0,0 +1,38 @@
+# print("any word you want")
+# # variables
+
+# # 2 things: its name, its value
+
+# my_variable_is_good = "word any"
+# myNumber = 1
+
+# # print(my_variable_is good) error
+# print(myNumber)
+
+# # interpreter
+# # compilator
+
+# print(12 + 3*4)
+
+# a = 5
+# b = 6
+
+# c = a*b
+
+# print(c) # 30
+
+# # comments
+
+'''
+dnkadn
+dhabdkkadn
+dkabdashbhb
+dabdadndaknfjdnksn
+dadkakdkdkfnksdnknfckdvfd
+'''
+
+# a = "just a word"
+# d = 3
+# myNumber = 10
+
+# Ctrl+/
