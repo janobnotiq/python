20d00a0938739dc5671dc21b6c3b464a2ab057af
Ulugbek <janobnotiq@gmail.com>
2025-02-09 13:00:17 +0500

    data types

---
+++ datatypes.py
@@ -0,0 +1,97 @@
+# print, variables
+
+myNumber = 1 # data
+myWord = "Python programming" # data
+
+# primitive data types
+
+# 4 four types: integer,string,float,boolean
+
+# integer
+
+points = 20
+
+# print(points)
+# print(type(points))
+# print(points + 3) # 23
+
+# float 1.0 is float, but 1 is ineteger
+
+rating = 4.7
+
+# print(type(rating))
+
+# boolean
+
+# 2 values: True, False
+
+is_open = True
+is_closed = False
+
+# print(type(is_open))
+
+# strings
+
+text = '... but the winds don\'t blow as the ships desire'
+city = 'Marhamat'
+
+# print(type(text))
+# print(text)
+
+# Ctrl+S
+
+# a = 3
+# print(a)
+# a = True
+# print(a)
+
+# type corversion
+# Ctrl+j
+
+# street_number = '314'
+# print(type(street_number)) # str
+# street_number = int(street_number)  # converting with int()
+# print(type(street_number)) # int
+# print(city)
+
+# bool --> str/int/float
+# int --> str/bool/float
+# str --> int/bool/float
+# float --> int/str/bool
+
+# int();bool();str();float()
+
+# int --> float   | float --> int
+
+# scores = 31
+# print(scores,type(scores))
+# scores = float(scores)
+# print(scores,type(scores))
+
+# efficiency = -3.3
+# print(efficiency,type(efficiency))
+# efficiency = int(efficiency)
+# print(efficiency,type(efficiency))
+
+# methods of string
+
+first_name = "uluGbEk"
+print(first_name)
+# print(first_name.endswith("k"))
+# print(first_name.startswith("u"))
+
+# U and u are different
+# Alt+up/down
+# print(first_name.lower())
+# print(first_name.upper())
+# print(first_name.capitalize())
+
+# case-sensetivity
+
+town = "Ukchi"
+Town = "Asaka"
+tOwn = "Karakurgan"
+
+print(town,Town,tOwn)
+
+# case-insensetivity  in telegram usernames t.me/ulugbek_aka  t.me/UlugBek_aka 
