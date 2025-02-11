578253a323c52963e6e1ff697d149b4e8592e011
Ulugbek <janobnotiq@gmail.com>
2025-02-11 18:05:07 +0500

    if statements 1

---
+++ ifstatements.py
@@ -0,0 +1,14 @@
+# if 10:
+#     print("Something")
+
+# # if statement:
+
+# # True or False
+# # bool(statement)
+# print(bool(10))
+
+number = -2
+
+if number > 0:
+    print("number is greater than 2")
+    print("positive number")
