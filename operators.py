12611f7a384a3a8ae57c85bf8eb6d57f84cb69ae
Ulugbek <janobnotiq@gmail.com>
2025-02-12 21:28:56 +0500

    operators, if,elif,else

---
+++ operators.py
@@ -0,0 +1,87 @@
+# arithmetic operators
+# +,-,*,/,//,**,%
+
+# +,-
+# a = 1
+# b = 2
+# print(a + b) # 3
+# print(a - b) # -1
+
+# *,/,//
+
+# x = 3
+# y = 10
+# print(x*y) # 30
+# print(x/y) # 0.3
+# print(x//y) # 0
+# print(y//x) # 3
+
+# n = -10
+# m = 3
+# print(m//n) # 0 should be -1
+# print(n//m) # -3 should be -4
+
+# **,%
+
+# print(3**3) # 3*3*3
+# print(4**5) # 4*4*4*4*4
+
+# print(20 % 3) # we use % to get remainder --> 2
+# 18:45
+
+# assignment operators
+# =,-=,+=,/=,//=,*=,**=,%=
+
+# a = "Ulugbek"
+# print(a)
+# a += "jon" # a = a + "jon" = "Ulugbek" + "jon"
+# print(a)
+
+# number = 40
+# print(number)
+# number %= 3 # number = number % 3 = 40 % 3 = 1
+# print(number)
+
+# comparision operators
+# >,<,==,!=,>=,<=
+# print(100 != 10)
+# print(10 >= 10)
+
+# 29:15
+# logical operations 
+# and, or, not
+
+# print(True and True and True and True and False)
+# # 1*1*1*1*1*0*1*1*1 = 0
+
+# print(True or False or False or False or False)
+# # 0 + 0 + 1 + 0 + 0 = 1
+
+# print(not True)
+# print(not False)
+
+# 36:05
+
+# if, else, elif
+# print("see what happens")
+
+# a = 1
+
+# if a > 10:
+#     print("number is greater than 10")
+#     print("number is greater than 10")
+#     print("number is greater than 10")
+#     print("number is greater than 10")
+#     print("number is greater than 10")
+# elif a == 10:
+#     print("number is ten")
+# else:
+#     print("number is less than 10")
+#     print("number is less than 10")
+
+# print("The end")
+
+isOpen = True
+
+# True --> "let's go"
+# False --> "stay at home"
